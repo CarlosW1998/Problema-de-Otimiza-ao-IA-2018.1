bb88700e78cea5f6fa6ed7d871a7084ef48bcc01
CarlosW1998 <carlos-walte@hotmail.com>
2018-07-23 21:43:28 -0300

    Hill climb Beta

--- hillclimbingBeta.c
+++ hillclimbingBeta.c
@@ -4,7 +4,7 @@
 #include<time.h>
 #define Euler 2.7182818284590452353
 #define debug 1
-int finalgain;
+double finalgain;
 //Passa os 3 angulos, devolve o ganho
 double gain(int phi1, int theta1, int phi2, int theta2, int phi3, int theta3);
 
@@ -13,8 +13,12 @@ double hillclimbing();
 
 int main()
 {
+  clock_t begin = clock();
   srand(time(NULL));
-  hillclimbing();
+  int a;
+  printf("%.2f\n", hillclimbing());
+  clock_t end = clock();
+  printf("%.2f\n", (double)((end - begin) / CLOCKS_PER_SEC));
   return 0;
 
 }
@@ -46,13 +50,7 @@ void recursionGenerate(int* modifyPath, int* finalPath, int a)
 {
   if(a == 6)
   {
-    if(debug)
-    {
-      int z;
-      for(z = 0; z < 6; z++) printf("%d ", modifyPath[z]);
-      printf("\n");
-    }
-    int thisgain = gain(modifyPath[0], modifyPath[1], modifyPath[2], modifyPath[3], modifyPath[4], modifyPath[5]);
+    double thisgain = gain(modifyPath[0], modifyPath[1], modifyPath[2], modifyPath[3], modifyPath[4], modifyPath[5]);
     if(thisgain > finalgain)
     {
       finalgain = thisgain;
@@ -63,7 +61,9 @@ void recursionGenerate(int* modifyPath, int* finalPath, int a)
   }
   modifyPath[a] += 1;
   recursionGenerate(modifyPath, finalPath, a+1);
-  modifyPath[a] -=2;
+  modifyPath[a] -=1;
+  recursionGenerate(modifyPath, finalPath, a+1);
+  modifyPath[a] -=1;
   recursionGenerate(modifyPath, finalPath, a+1);
   modifyPath[a] += 1;
 }
@@ -71,19 +71,27 @@ void recursionGenerate(int* modifyPath, int* finalPath, int a)
 
 double hillclimbing()
 {
-  int pathbestgain[6], auxpath[6], a;
-  double bestgain;
+  int pathbestgain[6], auxpath[6], a, random = 10000;
+  double bestgain, previous;
   for(a = 0; a < 6; a++)
   {
     pathbestgain[a] = rand()%360;
     auxpath[a] = pathbestgain[a];
   }
-  if(debug){
-    for(a = 0; a < 6; a++) printf("%d ", pathbestgain[a]);
-    printf("\n\n");
-  }
   bestgain = gain(pathbestgain[0] ,pathbestgain[1], pathbestgain[2], pathbestgain[3], pathbestgain[4], pathbestgain[5]);
   finalgain = bestgain;
-  recursionGenerate(auxpath, pathbestgain, bestgain);
-
+  while(random > 0)
+  {
+    if(rand()%1000 < random)
+    {
+      for(a = 0; a < 6; a++) auxpath[a] = rand()%360;
+    }
+    if(rand()%2) random--;
+    previous = bestgain;
+    recursionGenerate(auxpath, pathbestgain, 0);
+    for(a = 0; a < 6; a++) auxpath[a] = pathbestgain[a];
+    bestgain = finalgain;
+  }
+  for(a = 0; a < 3; a++)printf("%d %d\n", pathbestgain[a*2], pathbestgain[a*2+1]);
+  return bestgain;
 }
