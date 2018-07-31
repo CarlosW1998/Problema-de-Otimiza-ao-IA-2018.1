562ba0171cce736fee271e9f3450a50d6db22eb2
Carlos <carlos-walte@hotmail.com>
2018-07-31 16:56:58 -0300

    detalhes da comunicacao

--- request.py
+++ request.py
@@ -2,6 +2,8 @@ import requests
 
 k = open("mensagens.txt", "r")
 menssage = k.read()
+k.close()
 print(menssage)
-req = requests.get()
-print(req.text)
+req = requests.get(menssage[:len(menssage)-1])
+k = open("mensagens.txt", "w+")
+k.write(req.text)
--- server.c
+++ server.c
@@ -12,7 +12,7 @@ int main()
 
   srand(time(NULL));
   int a;
-  gain(90, 90, 90, 90, 90, 90);
+  gain(90, 90, 90, 90, 90, 60);
   clock_t end = clock();
   printf("%.2f\n", (double)(end - begin) / (double)CLOCKS_PER_SEC);
   return 0;
@@ -23,11 +23,13 @@ double gain(int phi1, int theta1, int phi2, int theta2, int phi3, int theta3)
 {
   char* message_fmt = "http://localhost:8080/antenna/simulate?phi1=%d&theta1=%d&phi2=%d&theta2=%d&phi3=%d&theta3=%d";
   char message[1024];
-  FILE* comunication = fopen("mensagens.txt", "w");
+  double result;
+  FILE* comunicationw = fopen("mensagens.txt", "w");
+  FILE* comunicationr = fopen("mensagens.txt", "r");
   sprintf(message,message_fmt,phi1,theta1,phi2,theta2,phi3,theta3);
-  fprintf(comunication, "%s", message);
-  fclose(comunication);
-  printf("%s\n", message);
-
+  //fprintf(comunicationw, "%s", message);
+  //system("python request.py");
+  fscanf(comunicationr, "%.f", &result);
+  printf("%f\n", result);
 
 }
